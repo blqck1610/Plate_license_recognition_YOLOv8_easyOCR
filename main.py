794103b31176e7efa7e391774dac1f4de968b337
Nguyen Tran <tdnguyen16102002@gmail.com>
2023-05-13 23:52:27 +0700

    update

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from tkinter import Tk
 from tkinter.filedialog import askopenfilename
 
+import cv2
+
 from ultralytics import YOLO
 
 Tk().withdraw()
-filename = askopenfilename() # sh
+filename = askopenfilename()
 model = YOLO("plate_license_mt.pt")
 results = model.predict(show=True,  source= filename)
+cv2.waitKey(0)
