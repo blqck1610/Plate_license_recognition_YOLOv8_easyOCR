a8d80cc018a237c4ccb8c39ceb6ec1c07dd643c8
Nguyen Tran <tdnguyen16102002@gmail.com>
2023-05-13 22:39:04 +0700

    add file train model by google colab

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ from ultralytics import  YOLO
 
 
 model = YOLO("plate_license_mt.pt")
-results = model.predict(show=True,  source="D:\\photo\\trung-bien-so-dep-nhieu-xe-o-to-doi-gia-vai-ty-dong-gay-sot.jpg")
+results = model.predict(show=True,  source= 0) #"C:\\Users\\USER\\Downloads\\test.MOV")
 cv2.waitKey(0)
